b0b4d3b6a1931db707501dac0024a146ff686f7d
guzelyaa <abitovaguzelyakilichbekovna@gmail.com>
2022-11-26 17:41:03 +0600

    Initial commit

---
+++ app/src/test/java/com/example/a5m7hwtest/MathTest.kt
@@ -0,0 +1,46 @@
+package com.example.a5m7hwtest
+
+import junit.framework.Assert.assertEquals
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+class MathTest {
+
+    var math: Math? = null
+
+    @Before
+    fun install() {
+        math = Math()
+    }
+
+    @Test
+    fun simpleAdd() {
+        assertEquals("6", math?.add("3", "3"))
+    }
+
+    @Test
+    fun checkEmpty() {
+        assertEquals("Вы не заполнили поле!", math?.add("", "4"))
+    }
+
+    @Test
+    fun checkNumber() {
+        assertEquals("Нельзя складывать буквы!", math?.add("ываыаы", "аывпып"))
+    }
+
+    @Test
+    fun dotAddCade(){
+        assertEquals("4.0", math?.add("2.0", "2.0"))
+    }
+
+    @Test
+    fun negativeAddCade(){
+        assertEquals("Нельзя складывать отрицательные числа!", math?.add("-2", "-2"))
+    }
+
+    @After
+    fun detach() {
+        math = null
+    }
+}
